4438694efcfa18026dea59f37b6d6fe301cdfaa8
Gregory Burgos <gregoryburgos17@gmail.com>
2024-01-20 17:18:43 -0400

    [add] funcion in js

--- odoo_view_btn_js/static/src/js/btn_views.js
+++ odoo_view_btn_js/static/src/js/btn_views.js
@@ -8,7 +8,7 @@ import { useService } from "@web/core/utils/hooks";
 export class Gbcontainer extends Component {
     setup(){
         this.state = useState({
-            task:{name:"WORD", value:5},
+            task:{name:"WORD", value:5,description:""},
             taskList:[],
             isEdit: false,
             activeId: false,
@@ -23,7 +23,54 @@ export class Gbcontainer extends Component {
 
     };
     async getAllTasks(){
-        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "value"])
+        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "value","description"])
+    }
+    addTask(){
+        this.resetForm()
+        this.state.activeId = false
+        this.state.isEdit = false
+    }
+
+    editTask(task){
+        this.state.activeId = task.id
+        this.state.isEdit = true
+        this.state.task = {...task}
+    }
+
+    async saveTask(){
+
+        if (!this.state.isEdit){
+            await this.orm.create(this.model, [this.state.task])
+            this.resetForm()
+        } else {
+            await this.orm.write(this.model, [this.state.activeId], this.state.task)
+        }
+
+        await this.getAllTasks()
+    }
+
+    resetForm(){
+        this.state.task = {name:"", value:0}
+    }
+
+    async deleteTask(task){
+        await this.orm.unlink(this.model, [task.id])
+        await this.getAllTasks()
+    }
+
+    async searchTasks(){
+        const text = this.searchInput.el.value
+        this.state.taskList = await this.orm.searchRead(this.model, [['name','ilike',text]], ["name", "value","description"])
+    }
+
+    async toggleTask(e, task){
+        await this.orm.write(this.model, [task.id], {description: e.target.checked})
+        await this.getAllTasks()
+    }
+
+    async updateValue(e, task){
+        await this.orm.write(this.model, [task.id], {value: e.target.value})
+        await this.getAllTasks()
     }
 
 
